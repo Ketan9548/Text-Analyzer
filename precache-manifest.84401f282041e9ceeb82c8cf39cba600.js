69e5b4de1bd772f4e6f03ef212199bbdf6328f17
ketan <98975157+Ketan9548@users.noreply.github.com>
2024-02-07 00:41:50 +0530

    done
    done

---
+++ precache-manifest.84401f282041e9ceeb82c8cf39cba600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3d1e383e43290c9fbd3f7360805e50",
+    "url": "/Text-Analyzer/index.html"
+  },
+  {
+    "revision": "9bd429d745c5562ae6c5",
+    "url": "/Text-Analyzer/static/css/main.2ce9df91.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Text-Analyzer/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Text-Analyzer/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "9bd429d745c5562ae6c5",
+    "url": "/Text-Analyzer/static/js/main.1db7a23b.chunk.js"
+  },
+  {
+    "revision": "89c388be00bf95e88cbe",
+    "url": "/Text-Analyzer/static/js/runtime~main.e3d9ecc6.js"
+  }
+]);
